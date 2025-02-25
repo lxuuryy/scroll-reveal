53030780891d1d79b336bc852173dc83a68f4698
akmalashwin <akmalashwin23@gmail.com>
2025-02-25 13:06:22 +1100

    first commit

---
+++ app/components/Text.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Text() {
+  return (
+    <div className="absolute flex flex-col text-[5.5vw] uppercase w-[80vw] items-start leading-tight">
+      <p>AKMAL ASHWIN</p>
+      <p className="self-end">FULL STACK Developer</p>
+      <p>Based in Melbourne</p>
+      <p className="self-end">Portfolio@2024</p>
+    </div>
+  )
+}
